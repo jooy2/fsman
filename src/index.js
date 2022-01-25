b96b550783ddef6099b9a7d0796f07f989c3e93f
leejooy96 <leejooy96@gmail.com>
2022-01-25 09:26:49 +0900

    add babel configuration and eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-const fs = require("fs");
-const winattr = require("winattr");
+const winattr = require('winattr');
 
-const isHidden = (path, isWindows) => {
-    return new Promise(resolve => {
-        if (isWindows) {
-            winattr.get(path, (error, data) => {
-                resolve(!error && data.hidden)
-            })
-        } else {
-            resolve(/(^|\/)\.[^/.]/.test(path));
-        }
-    })
-}
+const isHidden = (path, isWindows) => new Promise(resolve => {
+  if (isWindows) {
+    winattr.get(path, (error, data) => {
+      resolve(!error && data.hidden);
+    });
+  } else {
+    resolve(/(^|\/)\.[^/.]/.test(path));
+  }
+});
 
 module.exports = {
-    isHidden
-}
+  isHidden,
+};

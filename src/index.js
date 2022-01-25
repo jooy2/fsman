30ecba9137b7ffa77802fc9be54ec7dd71ddb0f1
leejooy96 <leejooy96@gmail.com>
2022-01-25 09:11:53 +0900

    is-hidden: use attr instead of node fs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
+const winattr = require("winattr");
 
 const isHidden = (path, isWindows) => {
     return new Promise(resolve => {
         if (isWindows) {
-            fs.open(path, 'w', err => {
-                resolve(err?.code === 'EPERM')
+            winattr.get(path, (error, data) => {
+                resolve(!error && data.hidden)
             })
         } else {
             resolve(/(^|\/)\.[^/.]/.test(path));

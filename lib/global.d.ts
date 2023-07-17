4a15f5e7f9401f31997193f7c0e72fdd628ff2bc
leejooy96 <leejooy96@gmail.com>
2023-07-17 19:11:48 +0900

    mod: remove unused interface

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -1,13 +1,6 @@
 declare module 'winattr';
 declare module 'fs';
 
-interface Attributes {
-	archive: boolean;
-	hidden: boolean;
-	readonly: boolean;
-	system: boolean;
-}
-
 interface FileStat {
 	success: boolean;
 	isDirectory: boolean;
